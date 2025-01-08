d3d48260492211d5cf3bec8f5cfe98aa14b74673
Antonio Di Mariano <antonio.dimariano@gmail.com>
2025-01-08 18:06:36 +0100

    [WIP] Add OptScale APIs for creating cloud accounts. Improve test files.

--- app/api/datasources/cloud_accounts/cloud_config_strategy.py
+++ app/api/datasources/cloud_accounts/cloud_config_strategy.py
@@ -1,7 +1,11 @@
 import logging
 from abc import ABC, abstractmethod
 
-from app.core.exceptions import CloudAccountConfigError
+from app.core.exceptions import (
+    CloudAccountConfigError,
+    OptScaleAPIResponseError,
+)
+from app.optscale_api.cloud_accounts import OptScaleCloudAccountAPI
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +21,20 @@ class CloudConfigStrategy(ABC):
                 logger.error(f"The {field} is required in the configuration")
                 raise CloudAccountConfigError(f"The {field} is required ")
 
-    @abstractmethod
-    def link_to_organization(self, config: dict, org_id: str):
-        pass
+    @staticmethod
+    async def link_to_organization(
+        config: dict,
+        org_id: str,
+        user_access_token: str,
+        cloud_account_api: OptScaleCloudAccountAPI,
+    ):  # noqa: E501
+        try:
+            response = await cloud_account_api.link_cloud_account(
+                user_access_token=user_access_token,  # noqa: E501
+                org_id=org_id,
+                conf=config,
+            )
+            return response
+
+        except OptScaleAPIResponseError as error:
+            raise OptScaleAPIResponseError from error
