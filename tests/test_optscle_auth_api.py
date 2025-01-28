9d4a6ab090fb45498913268dff26599a0b71e836
Antonio Di Mariano <antoniodimariano@users.noreply.github.com>
2025-01-28 19:09:28 +0100

    Mpt 6538 fix missing bearer token check proceeding with the creation of a cloud account (#30)
    * [WIP] Add initial logic for managing DataSources.
    Move the service's APIs and files to the API folder.
    Add Custom Exceptions to handle DataSources errors
    
    * Add initial check for validating the Bearer Token passed to the POST `modifier/v1/organizations/{org_id/cloud_accounts`.
     This prevents the service from returning a 403 with an indication about a not-allowed cloud type instead of processing the wrong authorization as the first thing.
    Change the error returned in the case of a not-allowed cloud type to 400
    
    * Rename valide_authorization to check_user_allowed_to_create_cloud_account
    Remove the returned response.
    Adjust tests

--- tests/test_optscle_auth_api.py
+++ tests/test_optscle_auth_api.py
@@ -25,6 +25,57 @@ def mock_get(mocker, opt_scale_auth):
     return mock_get
 
 
+async def test_authorize_valid_bearer_token(
+    async_client: AsyncClient, test_data: dict, mock_post, opt_scale_auth
+):
+    mock_response = test_data["auth_token"]["authorize"]["valid_response"]
+    mock_post.return_value = mock_response
+    response = await opt_scale_auth.check_user_allowed_to_create_cloud_account(
+        bearer_token="good token", org_id="my_org_id"
+    )
+    assert response is None
+    mock_post.assert_called_once_with(
+        endpoint="/auth/v2/authorize",
+        headers={
+            "Authorization": "Bearer good token",
+        },
+        data={
+            "action": "MANAGE_CLOUD_CREDENTIALS",
+            "resource_type": "organization",
+            "uuid": "my_org_id",
+        },
+    )
+
+
+async def test_authorize_bearer_token_with_error(
+    async_client: AsyncClient, mock_post, test_data: dict, opt_scale_auth, caplog
+):
+    mock_response = test_data["auth_token"]["authorize"]["error_response"]
+    mock_post.return_value = mock_response
+    with caplog.at_level(logging.ERROR):
+        with pytest.raises(APIResponseError) as exc_info:
+            await opt_scale_auth.check_user_allowed_to_create_cloud_account(
+                bearer_token="no good token", org_id="my_org_id"
+            )
+        exception = exc_info.value
+        assert exception.error.get("error_code") == "OA0010"
+        assert exception.error.get("reason") == "Token not found"
+        assert exception.error.get("status_code") == 401
+        # check the logging message printed by the obtain_user_auth_token_with_admin_api_key
+        assert "Failed validate the given bearer token" == caplog.messages[0]
+        mock_post.assert_called_once_with(
+            endpoint="/auth/v2/authorize",
+            headers={
+                "Authorization": "Bearer no good token",
+            },
+            data={
+                "action": "MANAGE_CLOUD_CREDENTIALS",
+                "resource_type": "organization",
+                "uuid": "my_org_id",
+            },
+        )
+
+
 async def test_user_auth_token_with_admin_api_key(
     async_client: AsyncClient, test_data: dict, mock_post, opt_scale_auth
 ):
